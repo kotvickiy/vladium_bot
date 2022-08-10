77cc14d7c164675cd310318fa2e0601437af3b89
kotvickiy <vladkotvickiy@gmail.com>
2022-08-10 11:31:42 +0500

    com

--- bot.py
+++ bot.py
@@ -7,7 +7,10 @@
 # ssh-copy-id vladium@myselfserver
 
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 from datetime import datetime
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
@@ -17,8 +20,40 @@ import os
 import socket
 
 
+storage = MemoryStorage()
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+
+def edit_phone_list(in_line):
+    flag = False
+    in_line = in_line.lower().split(" ")
+    if in_line[0] == "samsung" or in_line[0] == "s":
+        file = "../find_mobile/samsung.csv"
+    elif in_line[0] == "xiaomi" or in_line[0] == "x":
+        file = "../find_mobile/xiaomi.csv"
+    with open(file) as read_file:
+        lst_line = [i.strip().split(",") for i in read_file.readlines()]
+
+    for line in lst_line:
+        if line[0] == in_line[1]:
+            flag = True
+            if len(in_line) == 4:
+                line[1] = in_line[2]
+                line[2] = in_line[3]
+            elif len(in_line) == 2:
+                line[1] = 1000
+                line[2] = 100000
+
+    if flag:
+        with open(file, "w") as write_file:
+            for i in lst_line:
+                write_file.write(f"{i[0]},{i[1]},{i[2]}\n")
+        return "ACCEPT"
+
+
+class FSMAdmin(StatesGroup):
+    name = State()
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -65,10 +100,14 @@ b9 = KeyboardButton("Menu")
 b10 = KeyboardButton("Default cron")
 b11 = KeyboardButton("Out kwork")
 b12 = KeyboardButton("Out find_mobile")
-b13 = KeyboardButton("Clear")
+b13 = KeyboardButton("Edit")
+b14 = KeyboardButton("Clear")
+b15 = KeyboardButton("Cancel")
+b16 = KeyboardButton("Samsung")
+b17 = KeyboardButton("Xiaomi")
 
 
-acl = (CHAT_ID, )
+acl = (CHAT_ID, 5550131546)
 admin_only = lambda message: message.from_user.id not in acl
 
 
@@ -134,7 +173,7 @@ async def send(message : types.Message):
     elif message.text == "Menu":
         await message.delete()
         btn = verify_cron_and_install_button()
-        await bot.send_message(message.from_user.id, "Меню", reply_markup=kb().row(b1, b2).row(btn[0], btn[1]).row(b7, b8).row(b11, b12).row(b10, b13))
+        await bot.send_message(message.from_user.id, "Menu", reply_markup=kb().row(b1, b2).row(btn[0], btn[1]).row(b7, b8).row(b11, b12).row(b16, b17).row(b10, b13, b14))
     elif message.text == "Up kwork cron":
         await message.delete()
         btn = verify_cron_and_install_button()
@@ -190,6 +229,18 @@ async def send(message : types.Message):
         installation_crontab()
         new_btn = verify_cron_and_install_button()
         await bot.send_message(message.from_user.id, "/start", reply_markup=kb().row(new_btn[0], new_btn[1]).row(b9))
+    elif message.text == "Samsung":
+        with open("../find_mobile/samsung.csv") as file:
+            file = file.read()
+        await bot.send_message(message.from_user.id, file)
+        new_btn = verify_cron_and_install_button()
+        await bot.send_message(message.from_user.id, "/start", reply_markup=kb().row(new_btn[0], new_btn[1]).row(b9))
+    elif message.text == "Xiaomi":
+        with open("../find_mobile/xiaomi.csv") as file:
+            file = file.read()
+        await bot.send_message(message.from_user.id, file)
+        new_btn = verify_cron_and_install_button()
+        await bot.send_message(message.from_user.id, "/start", reply_markup=kb().row(new_btn[0], new_btn[1]).row(b9))
     elif message.text == "Clear":
         new_message_id = message.message_id
         for i in range(100):
@@ -198,6 +249,34 @@ async def send(message : types.Message):
             except Exception:
                 pass
             new_message_id = new_message_id - 1
+    elif message.text == "Edit":
+        await FSMAdmin.name.set()
+        await message.reply("Enter through the space: name(s or samsung) model max_price min_price", reply_markup=kb().row(b15))
+
+
+@dp.message_handler(Text(equals="Cancel", ignore_case=True), state="*")
+async def cancel_handler(message: types.Message, state: FSMContext):
+    curent_state = await state.get_state()
+    if curent_state is None:
+        return
+    await state.finish()
+    btn = verify_cron_and_install_button()
+    await message.reply("OK", reply_markup=kb().row(btn[0], btn[1]).row(b9))
+
+
+@dp.message_handler(state=FSMAdmin.name)
+async def load_model(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["name"] = message.text
+    btn = verify_cron_and_install_button()
+    try:
+        epl = edit_phone_list(data["name"])
+        await message.reply(epl, reply_markup=kb().row(btn[0], btn[1]).row(b9))
+    except:
+        await message.reply("NO RESULT", reply_markup=kb().row(btn[0], btn[1]).row(b9))
+        pass
+    
+    await state.finish()
 
 
 executor.start_polling(dp, skip_updates=True)
